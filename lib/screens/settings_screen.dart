274fa6b7dda0ae62d272c8f0df79010d958098d9
bscsproject <bscsproject33@gmail.com>
2026-08-16 12:29:33 +0500

    updated_profile

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -65,13 +65,6 @@ class SettingsScreen extends StatelessWidget {
           ),
 
           _SettingsTile(
-            icon: Icons.language_rounded,
-            title: 'Language',
-            subtitle: 'English',
-            onTap: () {},
-          ),
-
-          _SettingsTile(
             icon: Icons.lock_outline_rounded,
             title: 'Privacy',
             subtitle: 'Manage privacy settings',
